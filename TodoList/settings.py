a66d9b28956df41f4e0425db41b32f2f0e308be4
Pingwinos <31451550+Pingwinos@users.noreply.github.com>
2020-04-01 14:18:55 +0100

    Update settings.py

--- TodoList/settings.py
+++ TodoList/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'TodoList.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'templates/include')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
